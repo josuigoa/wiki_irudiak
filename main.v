32bcf292d8067ade086c26e377be1d0c60d11c9e
Josu Igoa <josu.igoa@indaba.lks.es>
2023-02-13 13:13:53 +0100

    magick-ekin testua sartu

--- main.v
+++ main.v
@@ -17,6 +17,7 @@ struct InfoBoxPage {
 	title     string
 	original  Media
 	imageinfo []Media
+	terms     Terms
 }
 
 struct Media {
@@ -24,6 +25,22 @@ struct Media {
 	url    string
 	width  int
 	height int
+}
+
+struct Terms {
+	label       []string
+	description []string
+}
+
+struct App {
+mut:
+	config      toml.Doc
+	title       string
+	description string
+	wiki_url    string
+	image_url   string
+	width       int
+	height      int
 }
 
 fn get_extension(img string) string {
@@ -35,9 +52,9 @@ fn check_size(config toml.Doc, width int, height int) bool {
 		&& width > height
 }
 
-fn get_info_box(config toml.Doc, title string) bool {
-	println('Looking infobox for ${title}')
-	infobox := '${config.value('wiki_api_endpoint').string()}&prop=pageimages|pageterms&piprop=original&titles=${title}'
+fn (mut app App) get_info_box() bool {
+	println('Looking infobox for ${app.title}')
+	infobox := '${app.config.value('wiki_api_endpoint').string()}&prop=pageimages|pageterms&piprop=original&titles=${app.title}'
 	res := http.get(infobox) or {
 		panic('Failed to get data, error: ${err}')
 		return false
@@ -48,15 +65,19 @@ fn get_info_box(config toml.Doc, title string) bool {
 		return false
 	}
 
+	if data.query.pages.len == 0 {
+		return false
+	}
+
+	app.description = data.query.pages[0].terms.description[0]
+
 	media := data.query.pages[0].original
 	extension := get_extension(media.source)
 
-	if check_size(config, media.width, media.height) && (extension == 'jpg' || extension == 'png') {
-		println('Downloading [${media.source}] image')
-		http.download_file(media.source, './img/${title}.${extension}') or {
-			panic('Failed to download image, error: ${err}')
-			return false
-		}
+	if check_size(app.config, media.width, media.height) && (extension == 'jpg') {
+		app.image_url = media.source
+		app.width = media.width
+		app.height = media.height
 
 		return true
 	} else {
@@ -64,9 +85,9 @@ fn get_info_box(config toml.Doc, title string) bool {
 	}
 }
 
-fn get_page(config toml.Doc, title string) bool {
-	println('Looking for ${title}')
-	page_info := '${config.value('wiki_api_endpoint').string()}&prop=imageinfo&generator=images&iiprop=url|size&titles=${title}'
+fn (mut app App) get_page() bool {
+	println('Looking for ${app.title}')
+	page_info := '${app.config.value('wiki_api_endpoint').string()}&prop=imageinfo&generator=images&iiprop=url|size&titles=${app.title}'
 	res := http.get(page_info) or {
 		panic('Failed to get data, error: ${err}')
 		return false
@@ -77,12 +98,10 @@ fn get_page(config toml.Doc, title string) bool {
 		return false
 	}
 
-	mut random_url := ''
 	mut count := 0
 
 	pages := data.query.pages.filter(it.imageinfo.len > 0
-		&& (get_extension(it.imageinfo[0].url) == 'jpg'
-		|| get_extension(it.imageinfo[0].url) == 'png'))
+		&& get_extension(it.imageinfo[0].url) == 'jpg')
 
 	if pages.len == 0 {
 		return false
@@ -90,8 +109,11 @@ fn get_page(config toml.Doc, title string) bool {
 	for {
 		index := rand.intn(pages.len) or { 0 }
 		media := pages[index].imageinfo[0]
-		if check_size(config, media.width, media.height) {
-			random_url = media.url
+		if check_size(app.config, media.width, media.height) {
+			app.image_url = media.url
+			app.width = media.width
+			app.height = media.height
+
 			break
 		}
 		count++
@@ -100,17 +122,15 @@ fn get_page(config toml.Doc, title string) bool {
 		}
 	}
 
-	println('Downloading [${random_url}] image')
-	http.download_file(random_url, './img/${title}.${get_extension(random_url)}') or {
-		panic('Failed to download image, error: ${err}')
-		return false
-	}
-
 	return true
 }
 
 fn main() {
-	config := toml.parse_file('./config.toml') or { panic(err) }
+	mut app := App{
+		config: toml.parse_file('./config.toml') or { panic(err) }
+		title: ''
+		image_url: ''
+	}
 
 	titles_json := os.read_file('./titles.json') or { '[]' }
 	titles := json.decode([]string, titles_json) or {
@@ -118,14 +138,36 @@ fn main() {
 		return
 	}
 
-	index := rand.intn(titles.len) or { 0 }
-	title := titles[index]
-	if !get_page(config, title) {
-		get_info_box(config, title)
+	for {
+		index := rand.intn(titles.len) or { 0 }
+		app.title = titles[index]
+		if !app.get_info_box() {
+			app.get_page()
+		}
+		if app.image_url != '' {
+			break
+		}
 	}
 
-	os.write_file('./.last_wiki_url', '${config.value('wiki_url').string()}${title}') or {
-		panic('Failed to write file, error: ${err}')
+	println('Downloading [${app.image_url}] image')
+	http.download_file(app.image_url, './img/${app.title}.${get_extension(app.image_url)}') or {
+		// http.download_file(app.image_url, app.config.value('out_file_path').string()) or {
+		panic('Failed to download image, error: ${err}')
 		return
+	}
+
+	wiki_url := '${app.config.value('wiki_url').string()}${app.title}'
+	img_msg := '${app.title}\\n${app.description}\\nIturria: ${wiki_url}'
+	if os.exists_in_system_path('magick') {
+		println('Inserting data in image')
+		pointsize := f32(app.width) * 0.015
+		annotate := '+20+${f32(app.height) * 0.03}'
+		img_magick_cmd := 'magick convert -fill white -pointsize ${pointsize} -gravity SouthEast -annotate ${annotate} "${img_msg}" ./img/${app.title}.jpg ./img/${app.title}.jpg'
+		os.execute(img_magick_cmd)
+	} else {
+		os.write_file('./.last_wiki_url', img_msg) or {
+			panic('Failed to write file, error: ${err}')
+			return
+		}
 	}
 }
--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
 # wiki_irudiak
 
+Aplikazio honen idea blokeo pantailan Euskal Herriko irudiak agertzea da.
+
+Horretarako, herrien wikipediako irudiak hartu eta deskargatzen dira. ImageMagick PATH-ean baldin badago, herriaren izena, deskribapena eta wikipediako URLa txertatzen ditu irudian bertan.
+
 eskerrik asko [zital](https://github.com/ZiTAL/udalerri-bot) herrien zerrendarengatik
